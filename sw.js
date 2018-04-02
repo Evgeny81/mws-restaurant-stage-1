2a9e91aebb0c889dea2d758f512fb22093f077bd
Кузнецов Евгений <ek027823@gmail.com>
2018-04-02 18:03:18 +0300

    Semantic fix, sw changed.

--- sw.js
+++ sw.js
@@ -1,52 +1,40 @@
-let staticCacheName = 'static-v1';
+const currentCacheName = 'static-v1';
 
-self.addEventListener('install', (event) => {
-    console.log('installed');
-    event.waitUntil(
-        caches.open(staticCacheName).then((cache) =>
-            cache.addAll([ // build cache, add initial resources
-                '/',
-                'index.html',
-                'restaurant.html',
-                'js/main.js',
-                'js/restaurant_info.js',
-                'js/dbhelper.js',
-                'css/styles.css',
-                '/data/restaurants.json',
-                'img/',
-            ])
-        ).then(() => self.skipWaiting())
-    );
+const allUrls = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(currentCacheName).then( cache => {
+        return cache.addAll(allUrls);
+    }).catch(err => {
+        console.log(err);
+    }));
 });
 
-self.addEventListener('activate', (event) => { // update cache, deleting data from cache
-    event.waitUntil(
-        caches.keys().then((cacheNames) =>
-            Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== staticCacheName) {
-                        return caches.delete(cacheName)
-                    }
-                })
-            )
-        )
-    );
-    return self.clients.claim();
+self.addEventListener('activate', event => {
+    event.waitUntil(caches.keys().then(cacheNames => {
+        cacheNames.filter( cacheName => {
+            return cacheName.startsWith('static-') && cacheName !== currentCacheName;
+        }).map( cacheName => {
+            return cache.delete(cacheName);
+        });
+    }));
 });
 
-self.addEventListener('fetch', (event) => { // respond with cache, retrieve from cache, network or database
-    event.respondWith(
-        // fetch(event.request)
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-        // caches.open(staticCacheName).then(cache => {
-        //     return cache.match(event.request).then(response => {
-        //         return response || fetch(event.request)
-        //                 .then(response => {
-        //                     cache.put(event.request, response.clone())
-        //                 })
-        //     })
-        // })
-    )
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.open(currentCacheName).then(cache => {
+        return cache.match(event.request).then(res => {
+            return res || fetch(event.request).then(res => {
+                cache.put(event.request, res.clone());
+                return res;
+            });
+        });
+    }));
 });
